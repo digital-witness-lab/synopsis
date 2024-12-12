9dffb04f937cfe5864452b3301575ba19ad66384
madelyne <madelyne.xiao96@gmail.com>
2024-12-12 09:20:50 -0500

    update run.py

--- scripts/run.py
+++ scripts/run.py
@@ -32,7 +32,7 @@ text_labels = []
 
 print("reading embedding file...")
 
-with open('test.txt.vecc', 'r') as f:
+with open('out.json', 'r') as f:
     for idx, line in enumerate(f):
         data = json.loads(line)
         embedding = data['embedding']
@@ -107,34 +107,34 @@ print("generating noised embeddings...")
 def sigma(epsilon, delta, c=1, w=1):
     return c * w * np.sqrt(2 * (np.log(1/(2*delta)) + epsilon)) / epsilon # log is ln
 
-eps3_noised_embeddings_five = []
+eps6_noised_embeddings_five = []
 
-five_noise_stddev_eps3 = sigma(3, 2**(-40), 1, 1/(np.sqrt(512)))
-five_noise_eps3 = np.random.normal(0, five_noise_stddev_eps3, 512)
+five_noise_stddev_eps6 = sigma(6, 2**(-40), 1, 1/(np.sqrt(512)))
+five_noise_eps6 = np.random.normal(0, five_noise_stddev_eps6, 512)
 
 for i in range(len(scaled_embeddings)):
 
-    five_noise_embed_eps3 = transformed_five[i] + five_noise_eps3
-    eps3_noised_embeddings_five.append(five_noise_embed_eps3)
+    five_noise_embed_eps6 = transformed_five[i] + five_noise_eps6
+    eps6_noised_embeddings_five.append(five_noise_embed_eps6)
 
 print("done.")
 
 
 # 3b ) find squared L2 distances between query vectors and noised embeddings 0.42 (for n = 512), 0.67 (for n = 256), > 1 (for n = 128)
 
-noised_five_dists_eps3 = []
+noised_five_dists_eps6 = []
 
 
 print("generating benchmark.tsv...")
 
 for i in range(len(embeddings)):
     
-    noised_five_dist_eps3 = distance.euclidean(transformed_five[0], eps3_noised_embeddings_five[i])
-    noised_five_dists_eps3.append(noised_five_dist_eps3**2)
+    noised_five_dist_eps6 = distance.euclidean(transformed_five[0], eps6_noised_embeddings_five[i])
+    noised_five_dists_eps6.append(noised_five_dist_eps6**2)
     
 
-df = pd.DataFrame(data=np.column_stack((ground_truths, scaled_dists_grounds, ep_fives, noised_five_dists_eps3)),
-                  columns=['ground_truth', 'ground_truth_scaled', 'five_transform', 	        'five_noised_eps3'])
+df = pd.DataFrame(data=np.column_stack((ground_truths, scaled_dists_grounds, ep_fives, noised_five_dists_eps6)),
+                  columns=['ground_truth', 'ground_truth_scaled', 'five_transform', 	        'five_noised_eps6'])
 
 df.to_csv('benchmarks.tsv', sep = '\t', index = None)
 
@@ -154,7 +154,7 @@ query_rep_df = pd.DataFrame(query_arr, index = None)
 
 query_rep_df.to_csv('query.tsv', index = None, sep = '\t', header = None)
 
-eps3_five = pd.DataFrame(eps3_noised_embeddings_five)
-eps3_five.to_csv('database.tsv', sep = '\t', index = None, header = False)
+eps6_five = pd.DataFrame(eps6_noised_embeddings_five)
+eps6_five.to_csv('database.tsv', sep = '\t', index = None, header = False)
 
 print("done.")
